be57beaed5d5c4da8b4929bb2879e66bdbc81e27
mthesoliver <m.limaoliveira96@gmail.com>
2024-09-01 17:21:37 -0300

    Adjustment of styles and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,14 @@
 import styles from "./page.module.scss";
 import Header from "Ead/Components/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className={styles.main}>
-        <div className={styles.description}>
+      <main className={styles.main + ' ' + styles.center}>
 
-        </div>
-
-        <div className={styles.center}>
-
-        </div>
-
-        <div className={styles.grid}>
-
-        </div>
-      </main>
+      </main >
     </>
   );
 }
