0794eece05f38b6a5098810ad13443dce6efcdf6
mthesoliver <m.limaoliveira96@gmail.com>
2024-09-01 17:20:43 -0300

    Configure header to pass icon to button and styles for navigation

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
+import Button from "./Button";
 import Navigation from "./Navigation";
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 
 function Header() {
+    const phoneNumber: string = '(11) 3136-0017';
+    const icon = <LocalPhoneOutlinedIcon />;
+
     return (
-        <header>
+        <header className="my-4 container-fluid d-flex justify-content-evenly align-items-center">
+            <Image src={'/assets/ead_main_logo.png'} alt='Logotipo da empresa Ead Plataforma' width={160} height={90} loading="eager"></Image>
             <Navigation />
+            <Button givenIcon={icon}>{phoneNumber}</Button>
         </header >
     )
 }
--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -1,9 +1,11 @@
+import styles from 'Ead/Styles/_navigation.module.scss';
+
 function Navigation() {
     return (
-        <nav>
-            <ul>
+        <nav className={styles.nav_bar}>
+            <ul className={'fw-light ' + styles.nav_list}>
                 <li><a href="/">Vantagens</a></li>
-                <li><a href="/">integrações</a></li>
+                <li><a href="/">Integrações</a></li>
                 <li><a href="/">Planos</a></li>
                 <li><a href="/">Blog</a></li>
                 <li><a href="/">Ajuda</a></li>
