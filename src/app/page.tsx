bf89dda155b39a6c514c6f251be0998acd8723b1
mthesoliver <m.limaoliveira96@gmail.com>
2024-09-01 19:21:49 -0300

    Small adjusts after insertion of YourCourseComponent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import styles from "./page.module.scss";
 import Header from "Ead/Components/Header";
+import YourCoursePlaceholder from "Ead/Components/YourCoursePlaceholder";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className={styles.main + ' ' + styles.center}>
-
+      <main className={styles.main}>
+        <YourCoursePlaceholder />
       </main >
     </>
   );
