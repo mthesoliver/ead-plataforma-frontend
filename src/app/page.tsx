b8cea298ec7916491969bdd48ec0d4daff297019
mthesoliver <m.limaoliveira96@gmail.com>
2024-09-07 18:07:18 -0300

    Create a SliderComponent and do some changes in another files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,8 @@ import DashboardCustomizeIcon from '@mui/icons-material/DashboardCustomize';
 import Diversity3Icon from '@mui/icons-material/Diversity3';
 import ImportantDevicesIcon from '@mui/icons-material/ImportantDevices';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Typography } from "@mui/material";
+import Carousel from "Ead/Components/SliderCustom";
 
 export default function Home() {
   return (
@@ -17,12 +19,19 @@ export default function Home() {
       <Header />
       <main className={styles.main}>
         <YourCoursePlaceholder />
-        <section id="video-placeholder">
+        <section id="video_container">
           <VideoPlaceholder videoId="0yrJU7b09Oc" />
-          <div className={styles.grid}>
-            <CardCustomBorder icon={Diversity3Icon} title="Criação de cursos coorporativos" content="Lorem ipsum dolor sit amet consectetur. Ipsum dolor facilisi eget nibh proin est dictum sit. Placerat." />
-            <CardCustomBorder icon={ImportantDevicesIcon} title="Venda seus cursos em nossa vitrine" content="Lorem ipsum dolor sit amet consectetur. Ipsum dolor facilisi eget nibh proin est dictum sit. Placerat." />
-            <CardCustomBorder icon={DashboardCustomizeIcon} title="Transforme sua plataforma para seu nicho" content="Lorem ipsum dolor sit amet consectetur. Ipsum dolor facilisi eget nibh proin est dictum sit. Placerat." />
+        </section>
+        <section id="cards_container">
+          <Typography gutterBottom className='fw-semibold mb-3 z3 position-relative ' sx={{ fontSize: 44, lineHeight: 1.4, textAlign: 'center', fontFamily: "Rajdhani" }} >
+            Veja as possibilidades com a EAD Plataforma
+          </Typography>
+          <div className={"z3 position-relative " + "styles.grid"}>
+            <Carousel>
+              <CardCustomBorder icon={ImportantDevicesIcon} title="Venda seus cursos em nossa vitrine" content="Lorem ipsum dolor sit amet consectetur. Ipsum dolor facilisi eget nibh proin est dictum sit. Placerat." />
+              <CardCustomBorder icon={Diversity3Icon} title="Criação de cursos coorporativos" content="Lorem ipsum dolor sit amet consectetur. Ipsum dolor facilisi eget nibh proin est dictum sit. Placerat." />
+              <CardCustomBorder icon={DashboardCustomizeIcon} title="Transforme sua plataforma para seu nicho" content="Lorem ipsum dolor sit amet consectetur. Ipsum dolor facilisi eget nibh proin est dictum sit. Placerat." />
+            </Carousel>
           </div>
         </section>
       </main >
