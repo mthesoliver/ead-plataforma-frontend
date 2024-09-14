b01244524ac17fa0e8142cb7e2e0257932c35d33
mthesoliver <m.limaoliveira96@gmail.com>
2024-09-14 20:44:04 -0300

    Creating and styling new componets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,7 @@ import Image from "next/image";
 import StampCircle from "Ead/Components/StampCircle";
 import InfiniteLogoScroll from "Ead/Components/InfiniteLogoScroll";
 import YouTube from "react-youtube";
+import CardTransparent from "Ead/Components/CardTransparent";
 
 export default function Home() {
   return (
@@ -110,8 +111,8 @@ export default function Home() {
           </section>
         </section>
 
-        <section id="light_container" className={"container gap-4 p-5 bg-light " + styles.full_width + ' ' + styles.align_to_top_el}>
-          <div className={"container d-flex flex-column my-5 " + styles.inner_contents}>
+        <section id="light_container" className={"container gap-4 p-5 bg-light h-50 " + styles.full_width + ' ' + styles.align_to_top_el}>
+          <div className={"container d-flex flex-column mt-5 " + styles.inner_contents}>
             <section id="clients_feedback_container" className={"d-flex flex-column align-items-center"}>
               <Typography gutterBottom className='fw-bolder mb-3 z3 position-relative ' sx={{ fontSize: 44, lineHeight: 1.4, textAlign: 'center', fontFamily: "Rajdhani", color: "#7C038E" }} >
                 Veja o que nossos cliente falam sobre nós
@@ -153,6 +154,80 @@ export default function Home() {
             </section>
           </div>
         </section >
+
+        <section id="features_container" className={"container d-flex flex-column gap-4 pt-5 mt-5"}>
+          <HeroTextCustom sub={"Confira nossas funcionalidades"} title={"Por que escolher a EAD plataforma?"} titleSize={60} align="left" />
+
+          <section id="features_overflow" className="container row">
+            <div className={"d-flex flex-row pt-5 m-auto gap-4"}>
+              <CardTransparent title="EAD LIVE"
+                subTitle="Deixe seus alunos mais engajados criando aulas ao vivo. Você pode disponibilizar um chat em tempo real para comunicação."
+                imagePath="/assets/images/card_icons/EAD_LIVE.png" size={3} />
+              <CardTransparent title="EAD Checkout"
+                subTitle="Ofereça opções customizadas de pagamentos, configure parcelamento, juros e vencimento do boleto por curso."
+                imagePath="/assets/images/card_icons/EAD_Checkout.png" size={3} />
+              <CardTransparent title="API Webhooks"
+                subTitle="Integre seu EAD com sistemas externos, automatize processos de cadastro do aluno, matrículas e outros."
+                imagePath="/assets/images/card_icons/API_Webhooks.png" size={3} />
+              <CardTransparent title="EAD Store"
+                subTitle="Lorem ipsum dolor sit amet consectetur. Vitae diam."
+                imagePath="/assets/images/card_icons/EAD_LIVE.png" size={3} />
+            </div>
+            <div className={"d-flex flex-row pt-5 m-auto gap-4"}>
+              <CardTransparent title="Assinatura e recorrência"
+                subTitle="Seu EAD como um Netflix! Crie planos de assinatura com cobranças mensais, trimestrais, semestrais ou anuais."
+                imagePath="/assets/images/card_icons/Assinatura_e_recorrência.png" size={3} />
+              <CardTransparent title="Grupos e turmas"
+                subTitle="Crie grupos e automatize o processo de cadastro e matrículas em seu EAD, importe até 1.000 alunos por vez."
+                imagePath="/assets/images/card_icons/Grupos_e_turmas.png" size={3} />
+              <CardTransparent title="Carrinho de Compras"
+                subTitle="Receba pagamentos através de cartão ou boleto com matrícula automática após confirmação de recebimento."
+                imagePath="/assets/images/card_icons/Carrinho_de_Compras.png" size={3} />
+              <CardTransparent title="Cupom de desconto"
+                subTitle="Ofereça cupons de desconto com até 100% do valor e matrícula automática no curso."
+                imagePath="/assets/images/card_icons/Cupom_de_desconto.png" size={3} />
+            </div>
+            <div className={"d-flex flex-row pt-5 m-auto gap-4"}>
+              <CardTransparent title="Aulas"
+                subTitle="Deixe seus alunos mais engajados criando aulas ao vivo. Você pode disponibilizar um chat em tempo real para comunicação."
+                imagePath="/assets/images/card_icons/Aulas.png" size={3} />
+              <CardTransparent title="Provas"
+                subTitle="Insira provas entre aulas e módulos, torne a prova requisito, programe a liberação com data fixa ou flexível, área de boletim."
+                imagePath="/assets/images/card_icons/Provas.png" size={3} />
+              <CardTransparent title="Certificados"
+                subTitle="Certificado de conclusão de curso com código de autenticação, envio automático para o e-mail do aluno."
+                imagePath="/assets/images/card_icons/Certificados.png" size={3} />
+              <CardTransparent title="Permissões"
+                subTitle="Determine funções administrativas customizadas para cada usuário, crie seus próprios padrões de permissões."
+                imagePath="/assets/images/card_icons/Permissões.png" size={3} />
+            </div>
+            <div className={"d-flex flex-row pt-5 m-auto gap-4"}>
+              <CardTransparent title="Multiplos professores"
+                subTitle="Adicione múltiplos professores em seus cursos e planos com pagamento automático da participação nas vendas."
+                imagePath="/assets/images/card_icons/Multiplos_professores.png" size={3} />
+              <CardTransparent title="Cobranças"
+                subTitle="Envie cobranças por e-mail com a automação de liberar cursos ou grupo, defina condições de pagamento e vencimento."
+                imagePath="/assets/images/card_icons/Cobranças.png" size={3} />
+              <CardTransparent title="Biblioteca"
+                subTitle="Você pode reaproveitar uma  aula em vários cursos, gerenciar conteúdo de maneira separada dos cursos."
+                imagePath="/assets/images/card_icons/Biblioteca.png" size={3} />
+              <CardTransparent title="Notificações"
+                subTitle="Acompanhe tudo o que acontece em seu perfil, seja notificado a cada venda, solicitação de suporte, correção de provas, novas aulas..."
+                imagePath="/assets/images/card_icons/Notificações.png" size={3} />
+            </div>
+            <div className={"d-flex flex-row pt-5 m-auto gap-4"}>
+              <CardTransparent title="Fórum professores"
+                subTitle="Fórum público para gerar conteúdos / Fórum fechado para  cada curso, apenas para  professores e alunos  matriculados.."
+                imagePath="/assets/images/card_icons/Fórum.png" size={3} />
+              <CardTransparent title="Domínio e SSL"
+                subTitle="Configure seu próprio domínio com hospedagem e certificado de segurança inclusos."
+                imagePath="/assets/images/card_icons/Domínio_e_SSL.png" size={3} />
+              <CardTransparent title="Personalize"
+                subTitle="Fácil configuração, tema customizável, painel administrativo para você mesmo gerenciar todas as áreas do EAD."
+                imagePath="/assets/images/card_icons/Assinatura_e_recorrência.png" size={3} />
+            </div>
+          </section>
+        </section>
 
       </main >
       <footer>
