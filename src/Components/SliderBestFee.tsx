6167e8f37614c2a6403eccf6fd79e5a87df928ff
mthesoliver <m.limaoliveira96@gmail.com>
2024-10-15 12:15:17 -0300

    Creating styles for fee component

--- src/Components/SliderBestFee.tsx
+++ src/Components/SliderBestFee.tsx
@@ -2,7 +2,7 @@
 
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Typography from '@mui/material/Typography';
 import styles from "Ead/Styles/_best-fee.module.scss";
 import { CardFeeContainerType, CardFeeInner, CardFeeInnerOthers } from 'Ead/Types/CardFee';
@@ -23,8 +23,8 @@ const CardFeeContentEad = ({ children, fee, aditional = '0.00' }: CardFeeInner) 
                 <img src={'/assets/images/ead_horizontal_footer_logo.png'} alt='Logotipo da Ead Plataforma' width={290} height={60} />
             </Box>
 
-            <Box className={'d-flex flex-column row my-2 text-center'}>
-                <Typography component={'p'} variant="body2" className='fw-light mt-3' sx={{ fontSize: 16, lineHeight: 1.6, color: '#fff' }}>
+            <Box className={'d-flex flex-column row text-center'}>
+                <Typography component={'p'} variant="body2" className='fw-light' sx={{ fontSize: 16, lineHeight: 1.6, color: '#fff' }}>
                     Valor de saque final:
                 </Typography>
 
@@ -42,7 +42,7 @@ const CardFeeContentEad = ({ children, fee, aditional = '0.00' }: CardFeeInner) 
                     R${aditional !== '0' ? aditional : '0.00'} + {fee}%
                 </Typography>
 
-                <Typography component={'p'} gutterBottom className='fw-light mt-3 m-auto d-flex flex-column' sx={{ fontSize: 16, lineHeight: "24px !important", color: '#fff', maxWidth: '320px!important' }} >
+                <Typography component={'p'} gutterBottom className='fw-light mt-2 m-auto d-flex flex-column' sx={{ fontSize: 16, lineHeight: "24px !important", color: '#fff', maxWidth: '320px!important' }} >
                     Oferecemos taxas mais competitivas que a concorrência. Compare e comprove!
 
                     <Box component={'span'} className='d-flex flex-row m-auto align-items-center mt-3'>
@@ -60,6 +60,9 @@ const CardFeeContentEad = ({ children, fee, aditional = '0.00' }: CardFeeInner) 
 const CardFeeContentOthers = ({ currentValue }: CardFeeInnerOthers) => {
     const { isMobile } = useResize();
     const [taxMethod, setTaxMethod] = useState(OtherTaxes.HOTMART);
+    const imgHotmart = useRef<HTMLImageElement>(null);
+    const imgKiwify = useRef<HTMLImageElement>(null);
+    const imgTicto = useRef<HTMLImageElement>(null);
 
     const additionalTaxesValues: number[] = [2.49, 1.00];
     const [additionalTaxes, setAdditionalTaxes] = useState<string>(additionalTaxesValues[0].toString());
@@ -82,6 +85,7 @@ const CardFeeContentOthers = ({ currentValue }: CardFeeInnerOthers) => {
         let finalWithdraw = taxesCalc(parseFloat(current), (taxMethod / 100), parseFloat(additionalTaxes));
         setFinalValue(formatCurrency(parseInt(finalWithdraw)));
 
+
     }, [currentValue, mainImage])
 
     const handleClick = (event: any) => {
@@ -92,16 +96,22 @@ const CardFeeContentOthers = ({ currentValue }: CardFeeInnerOthers) => {
                 setMainImage(imagesUrlPath[0]);
                 setTaxMethod(OtherTaxes.HOTMART);
                 setAdditionalTaxes(additionalTaxesValues[1].toString());
+
+                handleActive(imgHotmart, true, [imgKiwify, imgTicto]);
                 break;
             case 'Kiwify':
                 setMainImage(imagesUrlPath[1]);
                 setTaxMethod(OtherTaxes.KIWIFY);
                 setAdditionalTaxes(additionalTaxesValues[0].toString());
+
+                handleActive(imgKiwify, true, [imgHotmart, imgTicto]);
                 break;
             case 'Ticto':
                 setMainImage(imagesUrlPath[2]);
                 setTaxMethod(OtherTaxes.TICTO);
                 setAdditionalTaxes(additionalTaxesValues[0].toString());
+
+                handleActive(imgTicto, true, [imgHotmart, imgKiwify]);
                 break;
             default:
                 setMainImage(imagesUrlPath[0]);
@@ -109,14 +119,26 @@ const CardFeeContentOthers = ({ currentValue }: CardFeeInnerOthers) => {
         }
     };
 
+    const handleActive = (el: any, active: boolean, arr: any[]) => {
+        if (active) {
+            el.current!.classList.add(styles.active);
+            el.current!.classList.remove(styles.not_active);
+
+            arr.forEach((el: any) => {
+                el.current!.classList.remove(styles.active);
+                el.current!.classList.add(styles.not_active);
+            });
+        }
+    }
+
     return (
         <CardContent className={`d-flex flex-row row align-items-center justify-content-center col-6 `}>
             <Box className={`d-flex flex-row align-items-center justify-content-center`}>
                 <img src={mainImage} alt='Logotipo da Ead Plataforma' width={237} height={75} />
             </Box>
 
-            <Box className={'d-flex flex-column row my-2 text-center'}>
-                <Typography component={'p'} variant="body2" className='fw-light mt-3' sx={{ fontSize: 16, lineHeight: 1.6, color: '#fff' }}>
+            <Box className={'d-flex flex-column row text-center'}>
+                <Typography component={'p'} variant="body2" className='fw-light mt-2' sx={{ fontSize: 16, lineHeight: 1.6, color: '#fff' }}>
                     Valor de saque final:
                 </Typography>
 
@@ -134,11 +156,11 @@ const CardFeeContentOthers = ({ currentValue }: CardFeeInnerOthers) => {
                     R${additionalTaxes !== '1' ? additionalTaxes : '1.00'} + {taxMethod}%
                 </Typography>
 
-                <Box component={'div'} className='d-flex flex-row m-auto align-items-center mt-3'>
+                <Box component={'div'} className='d-flex flex-row m-auto align-items-center mt-2'>
                     <Stack component={'div'} className={`d-flex flex-row row`}>
-                        <img src={'/assets/images/concorrentes/Com bg/Botton_rates_hotmart.png'} alt="Hotmart" className={`d-flex flex-column col-4`} onClick={handleClick} style={{ cursor: 'pointer' }} />
-                        <img src={'/assets/images/concorrentes/Com bg/Botton_rates_kiwify.png'} alt="Kiwify" className={`d-flex flex-column col-4`} onClick={handleClick} style={{ cursor: 'pointer' }} />
-                        <img src={'/assets/images/concorrentes/Com bg/Botton_rates_ticto.png'} alt="Ticto" className={`d-flex flex-column col-4`} onClick={handleClick} style={{ cursor: 'pointer' }} />
+                        <img ref={imgHotmart} src={'/assets/images/concorrentes/Com bg/Botton_rates_hotmart.png '} alt="Hotmart" className={`d-flex flex-column col-4 ` + styles.active} onClick={handleClick} style={{ cursor: 'pointer' }} />
+                        <img ref={imgKiwify} src={'/assets/images/concorrentes/Com bg/Botton_rates_kiwify.png '} alt="Kiwify" className={`d-flex flex-column col-4 ` + styles.not_active} onClick={handleClick} style={{ cursor: 'pointer' }} />
+                        <img ref={imgTicto} src={'/assets/images/concorrentes/Com bg/Botton_rates_ticto.png '} alt="Ticto" className={`d-flex flex-column col-4 ` + styles.not_active} onClick={handleClick} style={{ cursor: 'pointer' }} />
                     </Stack>
                 </Box>
 
@@ -158,6 +180,12 @@ function SliderBestFee({ direction = 'row' }: Readonly<CardFeeContainerType>) {
     const additionalTaxesValues: number[] = [0.00, 0.10, 1.85]
     const [taxMethod, setTaxMethod] = useState(TaxMethod.PIX);
     const [additionalTaxes, setAdditionalTaxes] = useState<string>(additionalTaxesValues[0].toString());
+
+
+    const btnPix = useRef<HTMLButtonElement>(null);
+    const btnCredit = useRef<HTMLButtonElement>(null);
+    const btnCreditPrazo = useRef<HTMLButtonElement>(null);
+    const btnBoleto = useRef<HTMLButtonElement>(null);
 
     const [currentValue, setCurrentValue] = useState<string>('500000');
     const [finalValue, setFinalValue] = useState<string>(() => {
@@ -189,18 +217,26 @@ function SliderBestFee({ direction = 'row' }: Readonly<CardFeeContainerType>) {
             case 'pix':
                 setTaxMethod(TaxMethod.PIX);
                 setAdditionalTaxes(additionalTaxesValues[0].toString())
+
+                handleSelected(btnPix, true, [btnCredit, btnCreditPrazo, btnBoleto]);
                 break;
             case 'credito_vista':
                 setTaxMethod(TaxMethod.CREDIT_CARD_ON_DELIVERY);
                 setAdditionalTaxes(additionalTaxesValues[1].toString())
+
+                handleSelected(btnCredit, true, [btnPix, btnCreditPrazo, btnBoleto]);
                 break;
             case 'credito_prazo':
                 setTaxMethod(TaxMethod.CREDIT_CARD_INSTALLMENT);
                 setAdditionalTaxes(additionalTaxesValues[1].toString())
+
+                handleSelected(btnCreditPrazo, true, [btnPix, btnCredit, btnBoleto]);
                 break;
             case 'boleto':
                 setTaxMethod(TaxMethod.BOLETO);
                 setAdditionalTaxes(additionalTaxesValues[2].toString())
+
+                handleSelected(btnBoleto, true, [btnPix, btnCredit, btnCreditPrazo]);
                 break;
             default:
                 setTaxMethod(TaxMethod.PIX);
@@ -208,10 +244,20 @@ function SliderBestFee({ direction = 'row' }: Readonly<CardFeeContainerType>) {
         }
     };
 
+    const handleSelected = (el: any, active: boolean, arr: any[]) => {
+        if (active) {
+            el.current!.classList.add(styles.selected);
+
+            arr.forEach((el: any) => {
+                el.current!.classList.remove(styles.selected);
+            });
+        }
+    }
+
     return (
         <StyledEngineProvider injectFirst>
-            <Box className={`d-flex flex-row row w-100 m-auto mb-3 gap-3 `}>
-                <Card component={'button'} className={`d-flex flex-col ps-3  py-3 align-items-center justify-content-center ` + styles.card_item} variant="outlined" name='pix' onClick={handleMethod}>
+            <Box className={`d-flex flex-row row w-100 m-auto mb-3 gap-3 justify-content-between`}>
+                <Card component={'button'} ref={btnPix} className={`d-flex flex-col ps-3  py-3 align-items-center justify-content-center ` + styles.card_item + ' ' + styles.selected} variant="outlined" name='pix' onClick={handleMethod}>
                     <span className="me-2">
                         <PixIcon sx={{ color: '#fff', fontSize: '32px' }} />
                     </span>
@@ -220,7 +266,7 @@ function SliderBestFee({ direction = 'row' }: Readonly<CardFeeContainerType>) {
                     </Typography>
                 </Card >
 
-                <Card component={'button'} className={`d-flex flex-col ps-3  py-3  align-items-center justify-content-center ` + styles.card_item} variant="outlined" name='credito_vista' onClick={handleMethod}>
+                <Card component={'button'} ref={btnCredit} className={`d-flex flex-col ps-3  py-3  align-items-center justify-content-center ` + styles.card_item} variant="outlined" name='credito_vista' onClick={handleMethod}>
                     <span className="me-2">
                         <CreditCardIcon sx={{ color: '#fff', fontSize: '32px' }} />
                     </span>
@@ -229,7 +275,7 @@ function SliderBestFee({ direction = 'row' }: Readonly<CardFeeContainerType>) {
                     </Typography>
                 </Card >
 
-                <Card component={'button'} className={`d-flex flex-col ps-3  py-3   align-items-center justify-content-center ` + styles.card_item} variant="outlined" name='credito_prazo' onClick={handleMethod}>
+                <Card component={'button'} ref={btnCreditPrazo} className={`d-flex flex-col ps-3  py-3   align-items-center justify-content-center ` + styles.card_item} variant="outlined" name='credito_prazo' onClick={handleMethod}>
                     <span className="me-2">
                         <CreditCardIcon sx={{ color: '#fff', fontSize: '32px' }} />
                     </span>
@@ -238,7 +284,7 @@ function SliderBestFee({ direction = 'row' }: Readonly<CardFeeContainerType>) {
                     </Typography>
                 </Card >
 
-                <Card component={'button'} className={`d-flex flex-col ps-3  py-3  align-items-center justify-content-center ` + styles.card_item} variant="outlined" name='boleto' onClick={handleMethod}>
+                <Card component={'button'} ref={btnBoleto} className={`d-flex flex-col ps-3  py-3  align-items-center justify-content-center ` + styles.card_item} variant="outlined" name='boleto' onClick={handleMethod}>
                     <span className="me-2">
                         <ReceiptIcon sx={{ color: '#fff', fontSize: '32px' }} />
                     </span>
@@ -247,15 +293,15 @@ function SliderBestFee({ direction = 'row' }: Readonly<CardFeeContainerType>) {
                     </Typography>
                 </Card >
             </Box>
-            <Card className={`d-flex flex-column py-5 ` + styles.background_card} variant="outlined" >
+            <Card className={`d-flex flex-column py-3 ` + styles.background_card} variant="outlined" >
                 <Box className={`d-flex flex-row row m-auto w-100`}>
                     <Box>
-                        <Slider className={styles.slider_color} color='secondary' defaultValue={parseFloat(currentValue)} min={0} max={1000000} aria-label="Value slider" onChange={handleInputValues} step={100}></Slider>
+                        <Slider className={`mt-3 ` + styles.slider_color} color='secondary' defaultValue={parseFloat(currentValue)} min={0} max={1000000} aria-label="Value slider" onChange={handleInputValues} step={100}></Slider>
                     </Box>
                     <Typography component={'p'} variant="body2" className='fw-light mt-3 text-center' sx={{ fontSize: '16px', lineHeight: '1.6rem', color: '#fff' }}>
                         Arraste e veja os valores em vendas:
                     </Typography>
-                    <Typography component={'p'} variant="body2" className='fw-bold mt-3 text-center' sx={{ fontSize: '20px', lineHeight: '1.6rem', color: '#fff' }}>
+                    <Typography component={'p'} variant="body2" className='fw-bold text-center' sx={{ fontSize: '24px', lineHeight: '1.6rem', color: '#fff' }}>
                         R$ {currentValue},00
                     </Typography>
                 </Box>
