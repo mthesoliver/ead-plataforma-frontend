f7f7e6f2594391b20e7b88b2733f937b01179ac8
mthesoliver <m.limaoliveira96@gmail.com>
2024-10-19 21:35:26 -0300

    Setting styles on switch

--- src/Components/PersonalizePlataform.tsx
+++ src/Components/PersonalizePlataform.tsx
@@ -1,9 +1,10 @@
 
 'use client'
 
-import { FormControl, FormControlLabel, Radio, RadioGroup, Stack, StyledEngineProvider, Switch } from '@mui/material';
+import { Box, FormControl, FormControlLabel, Radio, RadioGroup, Stack, StyledEngineProvider, Switch } from '@mui/material';
 import useResize from 'Ead/CustomHooks/useResize';
 import { useEffect, useState } from 'react';
+import styles from 'Ead/Styles/_personalize-plataform.module.scss';
 
 function PersonalizePlataform() {
     const { isMobile } = useResize();
@@ -44,23 +45,45 @@ function PersonalizePlataform() {
 
     return (
         <StyledEngineProvider injectFirst>
-            <Stack component={'div'} direction={'row'}>
-                <FormControl>
+            <Box className={`d-flex flex-row position-relative `}>
+                {!isMobile ? (
+                    <Stack component={'div'} className={`d-flex flex-row row  `}>
+                        <FormControl className={`d-flex flex-row gap-${!isMobile ? 4 : 1} position-absolute ` + styles.controls_wrapper}>
+                            <Switch
+                                checked={theme === 'dark'}
+                                onChange={handleThemeToggle}
+                                inputProps={{ 'aria-label': 'controlled' }}
+                                className={styles.switch_theme}
+                            />
+                            <RadioGroup row onChange={handleChange} value={color} className={`d-flex flex-row`}>
+                                <FormControlLabel id="color_dot_1" value={0} control={<Radio />} label="" />
+                                <FormControlLabel id="color_dot_2" value={1} control={<Radio />} label="" />
+                                <FormControlLabel id="color_dot_3" value={2} control={<Radio />} label="" />
+                            </RadioGroup>
+                        </FormControl>
+                    </Stack>
+                ) : (
+                    <Stack component={'div'} className={`d-flex flex-row row  `}>
+                        <FormControl className={`d-flex flex-row gap-${!isMobile ? 4 : 1} position-absolute ` + styles.controls_wrapper}>
                     <Switch
                         checked={theme === 'dark'}
                         onChange={handleThemeToggle}
                         inputProps={{ 'aria-label': 'controlled' }}
+                                    className={styles.switch_theme}
                     />
-                    <RadioGroup row onChange={handleChange} value={color}>
-                        <FormControlLabel value={0} control={<Radio />} label="" />
-                        <FormControlLabel value={1} control={<Radio />} label="" />
-                        <FormControlLabel value={2} control={<Radio />} label="" />
+                                <RadioGroup row onChange={handleChange} value={color} className={`d-flex flex-row`}>
+                                    <FormControlLabel id="color_dot_1" value={0} control={<Radio />} label="" />
+                                    <FormControlLabel id="color_dot_2" value={1} control={<Radio />} label="" />
+                                    <FormControlLabel id="color_dot_3" value={2} control={<Radio />} label="" />
                     </RadioGroup>
                 </FormControl>
-            </Stack>
+                        </Stack>
+                )}
+
             <figure className="justify-content-center align-items-center">
                 <img src={currentImg} width={!isMobile ? 1276 : 1276 / 3} height={!isMobile ? 830 : 830 / 3} alt="Layout personalizado" />
             </figure>
+            </Box>
         </StyledEngineProvider>
     )
 }
