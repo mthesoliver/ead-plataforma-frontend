f41122c9965b59107c66ae0cb029d6be8c7a2819
mthesoliver <m.limaoliveira96@gmail.com>
2024-10-30 13:40:13 -0300

    Finalize the video modal

--- src/Components/VideoPlaceholder.tsx
+++ src/Components/VideoPlaceholder.tsx
@@ -4,8 +4,11 @@ import { Box, StyledEngineProvider } from '@mui/material';
 import styles from 'Ead/Styles/_video-placeholder.module.scss';
 import YouTube, { YouTubeEvent, YouTubeProps } from "react-youtube";
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import CancelIcon from '@mui/icons-material/Cancel';
+import { useEffect, useState } from 'react';
 
 function VideoPlaceholder(props: Readonly<{ videoId: string }>) {
+    const [openVideo, setOpenVideo] = useState(false);
     const stylesClass = styles.video_wrapper + ' ' + styles.video_overlay;
     const opts: YouTubeProps['opts'] = {
         height: '720',
@@ -16,20 +19,36 @@ function VideoPlaceholder(props: Readonly<{ videoId: string }>) {
         },
     };
 
-    const handleOnReady: YouTubeProps['onReady'] = (event: YouTubeEvent) => {
-        //event.target.pauseVideo();
+    const openModalVideo = () => {
+        setOpenVideo(true);
     }
+
+    const closeModalVideo = () => {
+        setOpenVideo(false);
+    }
+
+    useEffect(() => {
+    }, [openVideo]);
 
     return (
 
         <StyledEngineProvider injectFirst>
-            {/* <Box className={"text-center " + stylesClass}>
-                <YouTube videoId={props.videoId} options={opts} id={styles.video_content} onReady={handleOnReady} />
-            </Box> */}
+
+            {openVideo && (
+                <Box className={stylesClass}>
+                    <YouTube videoId={props.videoId} options={opts} id={styles.video_content} />
+
+                    <Box component={'a'} onClick={closeModalVideo} sx={{ cursor: 'pointer' }}>
+                        <CancelIcon sx={{ color: '#fff', zIndex: 99, fontSize: 44, top: 0, right: 120 }} className={'position-absolute '} />
+                    </Box>
+                </Box>
+            )}
+            <Box component={'a'} className={styles.link_wrapper} onClick={openModalVideo} sx={{ cursor: 'pointer' }}>
+                <PlayCircleOutlineIcon sx={{ color: '#fff' }} className={'position-absolute ' + styles.play_icon} />
+                <span className={styles.echo_icon}></span>
+            </Box>
             <Box component={"section"} className={'position-relative ' + styles.video_placeholder}>
                 <Box component={"div"} className={'position-absolute d-flex justify-content-center align-items-center m-auto '}>
-                    <PlayCircleOutlineIcon sx={{ color: '#fff' }} className={'position-absolute ' + styles.play_icon} />
-                    <span className={'position-absolute ' + styles.echo_icon}></span>
                     <img src='/assets/images/plataform_dash.png' className={styles.video_mock}></img>
                 </Box>
             </Box>
