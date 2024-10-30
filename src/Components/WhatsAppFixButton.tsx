9a9db0fdef5bfd17219e14e2c8ae46a23a895fe3
mthesoliver <m.limaoliveira96@gmail.com>
2024-10-30 12:21:36 -0300

    Refining some styles and icons animations

--- src/Components/WhatsAppFixButton.tsx
+++ src/Components/WhatsAppFixButton.tsx
@@ -13,7 +13,7 @@ function WhatsAppFixButton() {
         <StyledEngineProvider injectFirst>
             <Link href={'https://wa.me/551126266036?text=Estou%20interessado(a)%20em%20come%C3%A7ar%20meu%20EAD.'} target='_blank'>
                 <WhatsAppIcon sx={{ fontSize: !isMobile ? 62 : 56 }}
-                    style={{ position: 'fixed', bottom: '20px', right: '20px', zIndex: '99' }} className={styles.img_wrapper} />
+                    style={{ position: 'fixed', bottom: '20px', right: '20px', zIndex: '99' }} className={styles.img_wrapper + ' ' + styles.btn_wpp_border } />
             </Link>
         </StyledEngineProvider>
     )
