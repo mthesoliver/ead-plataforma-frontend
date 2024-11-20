ca6795e884841569d05d1f24bdcdb4bc06256a39
mthesoliver <m.limaoliveira96@gmail.com>
2024-11-20 16:31:57 -0300

    Adding text animations

--- src/Components/HeroTextCustom.tsx
+++ src/Components/HeroTextCustom.tsx
@@ -4,13 +4,54 @@ import { Box, StyledEngineProvider, Typography } from "@mui/material";
 import useResize from "Ead/CustomHooks/useResize";
 import styles from "Ead/Styles/_hero-text.module.scss";
 import { ColorMode, HeroTextType } from "Ead/Types/HeroTextType";
-import { useLayoutEffect, useState } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/src/ScrollTrigger';
 
 
 
 function HeroTextCustom({ title, sub, titleSize, colorMode, align }: Readonly<HeroTextType>) {
     const { isMobile } = useResize();
     const [mode, setColorMode] = useState<string[]>([styles.main_tittle_color, styles.sub_title]);
+    const heroText = useRef(null);
+
+    const ctaAnimation = () => {
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.from(heroText.current, {
+            y: 300,
+            position: "relative",
+            opacity: 0,
+            scrollTrigger: {
+                trigger: heroText.current,
+                start: "-650px 100px",
+                end: "50px  50px",
+                scrub: false,
+                //markers: true,
+            }
+        })
+        gsap.to(heroText.current, {
+            y: 0,
+            position: "relative",
+            opacity: 1,
+            scrollTrigger: {
+                trigger: heroText.current,
+                start: "-650px 100px",
+                end: "50px  50px",
+                scrub: false,
+                //markers: true,
+            }
+        });
+
+        return (() => {
+            gsap.killTweensOf(heroText.current);
+        });
+    }
+
+    useLayoutEffect(() => {
+        if (!isMobile) {
+            ctaAnimation();
+        }
+    }, [isMobile])
 
     useLayoutEffect(() => {
         handleColorMode(colorMode as ColorMode);
@@ -26,7 +67,7 @@ function HeroTextCustom({ title, sub, titleSize, colorMode, align }: Readonly<He
 
     return (
         <StyledEngineProvider injectFirst>
-            <Box className="my-4 d-flex justify-content-evenly flex-column">
+            <Box ref={heroText} className="my-4 d-flex justify-content-evenly flex-column">
                 <Typography component={'h4'} gutterBottom className={'fw-semibold ' + mode[1]} sx={{ fontSize: !isMobile ? 36 : 22, lineHeight: '2.2rem !important', textAlign: align ?? "center" }}>
                     {title}
                 </Typography>
