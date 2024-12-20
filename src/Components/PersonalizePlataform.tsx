51bec0a859e50586001ec132c0b4d39efd58f081
mthesoliver <m.limaoliveira96@gmail.com>
2024-12-20 10:10:13 -0300

    Removing animation on cards

--- src/Components/PersonalizePlataform.tsx
+++ src/Components/PersonalizePlataform.tsx
@@ -3,14 +3,11 @@
 
 import { Box, FormControl, FormControlLabel, Radio, RadioGroup, Stack, StyledEngineProvider, Switch } from '@mui/material';
 import useResize from 'Ead/CustomHooks/useResize';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from 'Ead/Styles/_personalize-plataform.module.scss';
-import { gsap } from 'gsap';
-import ScrollTrigger from 'gsap/src/ScrollTrigger';
 
 function PersonalizePlataform() {
     const { isMobile } = useResize();
-    const customContainer = useRef(null);
 
     const darkTheme = [
         '/assets/images/colorful_platform/colorful_platform_dark_purple.png',
@@ -43,49 +40,13 @@ function PersonalizePlataform() {
         }
     }
 
-    const simpleCardAnimation = () => {
-        gsap.registerPlugin(ScrollTrigger);
-        gsap.from(customContainer.current, {
-            x: 1200,
-            position: "relative",
-            opacity: 0,
-            scrollTrigger: {
-                trigger: customContainer.current,
-                start: `-150px 300px`,
-                end: `-50px  600px`,
-                scrub: false,
-                //markers: true,
-            }
-        })
-        gsap.to(customContainer.current, {
-            x: 0,
-            position: "relative",
-            opacity: 1,
-            scrollTrigger: {
-                trigger: customContainer.current,
-                start: `-150px 300px`,
-                end: `-50px  600px`,
-                scrub: false,
-                //markers: true,
-            }
-        })
-
-        return (() => {
-            gsap.killTweensOf(`.card_simple`);
-        });
-    }
-
     useEffect(() => {
-
-        if (!isMobile) {
-            simpleCardAnimation();
-        }
         handleCurrentImg();
     }, [color, theme, isMobile])
 
     return (
         <StyledEngineProvider injectFirst>
-            <Box ref={customContainer} className={`d-flex flex-row position-relative `}>
+            <Box className={`d-flex flex-row position-relative `}>
                 <img src="../assets/images/arrow/arrow_curve_banner.svg" className={'position-absolute ' + styles.arrow}></img>
                 {!isMobile ? (
                     <Stack component={'div'} className={`d-flex flex-row row  `}>
